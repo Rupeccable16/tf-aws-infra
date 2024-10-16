2c8c1085b57c816cd37862779393afa8b6857805
RupeshRokade16 <rokade.r@northeastern.edu>
2024-10-16 15:46:18 -0400

    add ec2

--- ec2.tf
+++ ec2.tf
@@ -9,24 +9,24 @@ data "aws_ami" "latest_ami" {
 
 
 
-# resource "aws_instance" "my-ec2" {
-#   depends_on                  = [aws_vpc.csye6225_vpc]
-#   ami                         = data.aws_ami.latest_ami.id
-#   instance_type               = "t2.small"
-#   subnet_id                   = aws_subnet.public-subnet-1.id
-#   associate_public_ip_address = true
+resource "aws_instance" "my-ec2" {
+  depends_on                  = [aws_vpc.csye6225_vpc]
+  ami                         = data.aws_ami.latest_ami.id
+  instance_type               = "t2.small"
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  associate_public_ip_address = true
 
-#   root_block_device {
-#     delete_on_termination = true
-#     volume_size           = 25
-#     volume_type           = "gp2"
-#   }
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 25
+    volume_type           = "gp2"
+  }
 
-#   disable_api_termination = false
+  disable_api_termination = false
 
-#   vpc_security_group_ids = [aws_security_group.application_security_group.id]
+  vpc_security_group_ids = [aws_security_group.application_security_group.id]
 
-#   tags = {
-#     Name = "my-ec2"
-#   }
-# }
+  tags = {
+    Name = "my-ec2"
+  }
+}
